900d405db0aced52f0ac3ca50546d6bc4c4ae18a
Vincent Boutour <bob@vibioh.fr>
2024-06-27 10:51:07 +0200

    fix: Fixing submux strip prefix
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/kaamebott/port.go
+++ cmd/kaamebott/port.go
@@ -7,10 +7,13 @@ import (
 func newPort(config configuration, services services) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.Handle("/slack/", services.slack.NewServeMux())
-	mux.Handle("/discord/", services.discord.NewServeMux())
+	mux.Handle("/slack/", http.StripPrefix("/slack", services.slack.NewServeMux()))
+	mux.Handle("/discord/", http.StripPrefix("/discord", services.discord.NewServeMux()))
 
-	mux.Handle(config.renderer.PathPrefix+"/", services.renderer.NewServeMux(services.search.TemplateFunc))
+	mux.Handle(config.renderer.PathPrefix+"/", http.StripPrefix(
+		config.renderer.PathPrefix,
+		services.renderer.NewServeMux(services.search.TemplateFunc),
+	))
 
 	return mux
 }
